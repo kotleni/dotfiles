5fd2b7cc9878b824b6ac2f8399fca59018849511
Viktor Varenik <yavarenikya@gmail.com>
2026-04-26 23:31:51 +0300

    chore: add nvim and tmux configs

---
+++ .config/nvim/init.lua
@@ -0,0 +1,115 @@
+local cfgutils = require('cfgutils')
+
+cfgutils.setTabWidth(4)
+vim.o.expandtab = true
+
+-- transparent background
+local highlight_groups = {
+    "Normal", "NormalNC", "SignColumn", "LineNr", "EndOfBuffer", "MsgArea"
+}
+for _, group in ipairs(highlight_groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+end
+vim.opt.termguicolors = true
+
+vim.g.mapleader = ','
+vim.o.number = true
+vim.o.mouse = ''
+vim.o.showmode = false
+vim.o.breakindent = true
+vim.o.cursorline = true
+vim.opt.termguicolors = true
+
+-- minimal number of screen lines to keep above and below the cursor
+vim.o.scrolloff = 10
+
+-- case insensitive searching unless \C or no one capital letter is present
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- set character to show some whitespace characters
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- sync clipboard between OS and nvim
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
+-- load lazy vim
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
+end
+
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+require('lazy').setup({
+    {
+        'nvim-telescope/telescope.nvim',
+        enabled = true,
+        event = 'VimEnter',
+        requires = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+    }
+})
+
+vim.lsp.enable('pyright')
+
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function() pcall(vim.treesitter.start) end,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        vim.o.signcolumn = 'yes:1'
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if client:supports_method('textDocument/completion') then
+            vim.o.complete = 'o,.,w,b,u'
+            vim.o.completeopt = 'menu,menuone,popup,noinsert'
+            vim.lsp.completion.enable(true, client.id, args.buf)
+        end
+    end
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function() vim.highlight.on_yank() end,
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+vim.api.nvim_set_keymap("n", "<leader>t", ":Neotree toggle<CR>", { noremap = true, silent = true })
+
+-- auto-close neotree on select
+vim.api.nvim_create_autocmd('BufEnter', {
+  callback = function()
+    if vim.bo.buftype == '' then
+      vim.cmd('Neotree close')
+    end
+  end,
+})
+
+
