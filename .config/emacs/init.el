42ae206242100ce1f38c634aafef25db7b7de932
Viktor Varenik <yavarenikya@gmail.com>
2026-05-30 16:43:04 +0300

    chore(emacs): set font to iosevka

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -15,6 +15,10 @@
 
 (setq inhibit-startup-screen t)
 
+; font
+(add-to-list 'default-frame-alist
+             '(font . "Iosevka Nerd Font Mono"))
+
 ; visual
 (load-theme 'modus-vivendi)
 (setopt
@@ -43,15 +47,3 @@
 
 ; auto-reload changed files from disk 
 (global-auto-revert-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
